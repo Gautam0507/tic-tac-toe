02b31a787de3779bacd620cef3d6fb9ec15eb931
Gautam0507 <110854761+Gautam0507@users.noreply.github.com>
2023-05-30 23:21:00 +0530

    added the winning logic

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ window_width = 400
 window_height = 400
 white = (255, 255, 255)
 line_color = (0, 0, 0)
-baord = [[None]*3, [None]*3, [None]*3]
+board = [[None]*3, [None]*3, [None]*3]
 
 pygame.init()
 fps = 30
@@ -59,6 +59,41 @@ def draw_status():
     text = font.render(message, 1, (255, 255, 255))
 
     screen.fill((0, 0, 0), (0, 400, 500, 100))
-    text_rect = text.get_rect(center = (window_width / 2, 500-50))
+    text_rect = text.get_rect(center=(window_width / 2, 500-50))
     screen.blit(text, text_rect)
-    pygame.display.update
+    pygame.display.update()
+
+# Main Tic_tac_toe logic
+
+
+def check_win():
+    global board, winner, draw
+
+    for row in range(0, 3):
+        if ((board[row][0] == board[row][2] == board[row][1]) and (board[row][0] is not None)):
+            winner = board[row][0]
+            pygame.draw.line(screen, (250, 0, 0), (0, (row + 1)*window_height / 3 - window_height / 6),
+                             (window_width, (row + 1)*window_height / 3 - window_height / 6), 4)
+            break
+
+    for col in range(0, 3):
+        if ((board[0][col] == board[1][col] == board[2][col]) and (board[row][0] is not None)):
+            winner = board[0][col]
+            pygame.draw.line(screen, (250, 0, 0), ((col + 1)*window_width / 3 - window_width / 6, 0),
+                             ((col + 1)*window_width / 3 - window_width / 6, window_height), 4)
+            break
+
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
+
+        winner = board[0][0]
+        pygame.draw.line(screen, (250, 70, 70), (50, 50), (350, 350), 4)
+
+    if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
+
+        winner = board[0][2]
+        pygame.draw.line(screen, (250, 70, 70), (350, 50), (50, 350), 4)
+
+    if(all([all(row) for row in board]) and winner is None):
+        draw = True
+    draw_status()
+    
